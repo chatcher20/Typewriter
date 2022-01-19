98e77328e7302a14abe654a58b4b45f1d4067d8f
Chris Hatcher <hatcherchris20@gmail.com>
2022-01-19 01:23:37 +0000

    Initial commit.

---
+++ hello_timeout.js
@@ -0,0 +1,23 @@
+setTimeout(() => {
+  console.log('4311o' + ' ' + 'th3r3' + ' ' + 'w0r1d');
+}, 4000);
+
+setTimeout(() => {
+  console.log('4311o');
+}, 1000);
+
+setTimeout(() => {
+  console.log('th3r3');
+}, 2000);
+
+setTimeout(() => {
+  console.log('w0r1d');
+}, 3000);
+
+
+
+
+
+
+
+
